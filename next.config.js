3cdcf2e159701183f85b7f416b83366f8bfd3998
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-14 20:33:16 +0900

    chore: config調整

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  optimizeFonts: true,
+  reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  },
 };
 
 module.exports = nextConfig;
