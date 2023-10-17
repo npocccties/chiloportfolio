1b7dd8a3c2b1a6245689492938e001f5286ec069
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-17 18:35:29 +0900

    chore: basePath / assetPath の調整

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? `/e-portfolio` : undefined,
+  basePath: isProd ? `/e-portfolio` : undefined,
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
