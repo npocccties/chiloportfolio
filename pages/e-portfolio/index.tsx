0f16beb1f81ee109461c5dff3c98f58ab7aa54e0
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-17 17:33:59 +0900

    Revert "chore: 試しにルートにindexを移動してみる"
    This reverts commit 8fbcbdab9cfdc5f5bc7d4efb812279fcbcef306e.

--- pages/e-portfolio/index.tsx
+++ pages/e-portfolio/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from "next";
 
 import { Layout } from "@/components/Layout";
 import { Metatag } from "@/components/Metatag";
-import { Portfolio } from "../components/e-portfolio/Portfolio";
+import { Portfolio } from "../../components/e-portfolio/Portfolio";
 
 const serviceName = process.env.NEXT_PUBLIC_SERVICE_NAME as string
 const serviceDescription = process.env.NEXT_PUBLIC_SERVICE_DESCRIPTION as string
