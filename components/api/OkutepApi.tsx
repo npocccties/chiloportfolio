9e4859d6b9dc9e3a0b7c3dcf880c4d3575a87a65
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-17 10:24:23 +0900

    chore: ログ調整

--- components/api/OkutepApi.tsx
+++ components/api/OkutepApi.tsx
@@ -5,6 +5,7 @@ const baseUrl = process.env.NEXT_PUBLIC_OKUTEP_BASE_URL as string
 export function useConsumerBadgesList (invisible: boolean) {
   const apiPath = `/api/v1/consumer/badges/list/?invisible=${invisible}`
   const url = `${baseUrl}${apiPath}`
+  console.log(url)
   async function fetcher(key: string, init?: RequestInit) {
     return fetch(key, init).then((res) => res.json() as Promise<ConsumerBadges[] | null>)
   }
