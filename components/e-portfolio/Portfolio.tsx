7699a03538ff204fda993b53a8f6e7dc270594cd
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-17 14:43:41 +0900

    Revert "chore: 試しにAxiosでデータ取得してみて開発サーバーでもデータ取得できるか確認する"
    This reverts commit 81b8840e8e44674cc7c403d2d93779131c7796d6.

--- components/e-portfolio/Portfolio.tsx
+++ components/e-portfolio/Portfolio.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { SelectConsumer } from "../ui/SelectConsumer"
-import { getJSON, useConsumerBadgesList } from "@/components/api/OkutepApi"
+import { useConsumerBadgesList } from "@/components/api/OkutepApi"
 import { useWalletBadgeList } from "@/components/api/WalletApi"
 import { getCsvText, mergeBadgeData } from "@/components/util/Converter"
 import { Button, FormLabel, Link, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Text, useDisclosure, Box, Flex, HStack, Spinner } from "@chakra-ui/react"
@@ -10,24 +10,146 @@ import { useForm } from "react-hook-form"
 import jconv from "jconv"
 import { BiKey } from "react-icons/bi";
 import { PortfolioBadgeData } from "@/components/data/PortfolioData"
-import axios from "axios"
 const csvFileName = process.env.NEXT_PUBLIC_CSV_FILE_NAME as string
 
 export const Portfolio = () => {
 
-  const [json, setJson] = useState(null)
-  React.useEffect(() => {
-    const url = "https://yesno.wtf/api";
-    axios.get(url).then((response) =>{
-      setJson(response.data)
-      console.log(response.data)
-    })
-  }, []);
+  const [validPassword, setValidPassword] = useState(false)
+  // OKUTEPからバッジ情報の取得
+  const { consumerBadges, isLoading, isError } = useConsumerBadgesList(validPassword)
 
-  if (!json) return <Spinner/>
+  // BadgeWalletからバッジ情報の取得
+  const { walletBadges, isLoadingWB, isErrorWB } = useWalletBadgeList()
+
+  // OKUTEPとBadgeWalletのバッジ情報をマージ
+  var portfolioBadges: PortfolioBadgeData[] = []
+  if (consumerBadges && walletBadges) {
+    portfolioBadges = mergeBadgeData(consumerBadges, walletBadges)
+  }
+  // 教員育成指標のプルダウン選択時のハンドラ
+  const [selectedConsumer, setSelectedConsumer] = useState('')
+  const [selectedFramework, setSelectedFramework] = useState('')
+  const [selectedStage, setSelectedStage] = useState('')
+  const onChangeConsumer = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const array = e.target.value.split(' ')
+    const consumerName = array[0]
+    const frameworkName = array[1]
+    const stageName = array[2]
+    console.log(`consumerName: ${consumerName} frameworkName: ${frameworkName} stageName: ${stageName}`)
+    setSelectedConsumer(consumerName)
+    setSelectedFramework(frameworkName)
+    setSelectedStage(stageName)
+  }
+
+  // キー入力のダイアログとの連携
+  const { isOpen, onClose, onOpen } = useDisclosure()
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<KeyInputForm>()
+
+
+  const consumers = new Set<string>()
+  portfolioBadges.map(v => {
+    if (!v.stage_invisible || (validPassword && v.stage_invisible)) {
+      consumers.add(getKeyName(v))
+    }
+  })
+  console.log('consumers: ', consumers)
+
+  // CSVダウンロード
+  const onCsvDownload = () => {
+    var targets = portfolioBadges.filter(v => v.consumer_name == selectedConsumer)
+    var text = getCsvText(targets)
+    text = jconv(text, "UTF8", "Shift_JIS")
+    const blob = new Blob([text], {type: 'text/csv'})
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement("a")
+    document.body.appendChild(a)
+    a.download = csvFileName
+    a.href = url
+    a.click()
+    a.remove()
+    URL.revokeObjectURL(url)    
+  }
+
+  const onKeyInputClosed = () => {
+    // 教員育成指標のプルダウンを選択解除する
+    var elements = document.getElementsByName('consumer')
+    for (let i = 0; i < elements.length; i++){
+      var obj = elements[i] as HTMLSelectElement;
+      obj.selectedIndex = 0;
+    }
+    setSelectedConsumer('')
+    setSelectedFramework('')
+    setSelectedStage('')
+  }
+
+  if (isLoading || isLoadingWB) return <Spinner/>
+  if (isError || isErrorWB) return <div>failed to load</div>
   return (
     <>
-    OK
+      {/** desktop */}
+      <Flex
+        display={{ base: "none", sm: "flex" }}
+        w="full"
+        justify={"space-between"}
+        direction={"row"}
+        alignItems={"flex-end"}
+      >
+        <Box mt={4}>
+          <FormLabel mb={2} fontSize={"md"}>
+            教員育成指標選択
+          </FormLabel>
+          <HStack>
+            <SelectConsumer consumers={Array.from(consumers)} handleChange={onChangeConsumer} />
+            <Link onClick={onOpen}>
+              <Text fontSize='40px'><BiKey/></Text>
+            </Link>
+          </HStack>
+        </Box>
+        <Box mt={4}>
+          <Button colorScheme='blue' onClick={onCsvDownload}>CSVダウンロード</Button>
+        </Box>
+      </Flex>
+
+      {/** smart phone */}
+      <Flex
+        display={{ base: "flex", sm: "none" }}
+        w="full"
+        justify={"space-between"}
+        direction={"column"}
+        alignItems={"center"}
+      >
+        <Box w={"full"} mt={7}>
+          <FormLabel mb={2} fontSize={"md"}>
+            教員育成指標選択
+          </FormLabel>
+          <HStack>
+            <SelectConsumer consumers={Array.from(consumers)} handleChange={onChangeConsumer} />
+            <Link onClick={onOpen}>
+              <Text fontSize='40px'><BiKey/></Text>
+            </Link>
+          </HStack>
+        </Box>
+        <Box w={"full"} mt={8}>
+          <Button w={"full"} colorScheme='blue' onClick={onCsvDownload}>CSVダウンロード</Button>
+        </Box>
+      </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>取得キー入力</ModalHeader>
+          <ModalBody>
+            <KeyInput register={register} watch={watch} handleSubmit={handleSubmit} onClose={onClose} setValidPassword={setValidPassword} onKeyInputClosed={onKeyInputClosed}/>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <BadgeList portfolioBadges={portfolioBadges} selectedConsumer={selectedConsumer} selectedFramework={selectedFramework} selectedStage={selectedStage}/>
     </>
   )
 }
