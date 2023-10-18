eb47eb6285410759c9d1e5e3f2f3a61246fcd9c6
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-18 09:09:34 +0900

    chore: config調整

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const urlPrefix = `/e-portfolio`
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: urlPrefix,
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
