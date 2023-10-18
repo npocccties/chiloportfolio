d58e02e373da3dfb9597bab1dab998d6c846f4ab
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-18 18:00:31 +0900

    chore: e-portfolioのURLパスを一旦取りやめ

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '/e-portfolio/', // assetPrefix requires the trailing slash
-  basePath: '/e-portfolio',
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
