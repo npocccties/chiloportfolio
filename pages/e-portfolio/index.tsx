02d8566a8184d559aa43e3db163bd04e886ef300
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-18 17:33:02 +0900

    chore: 何やらページ自体が404エラーになったので対処してみる

---
+++ pages/e-portfolio/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import type { NextPage } from "next";
+
+import { Layout } from "@/components/Layout";
+import { Metatag } from "@/components/Metatag";
+import { Portfolio } from "../../components/e-portfolio/Portfolio";
+
+const serviceName = process.env.NEXT_PUBLIC_SERVICE_NAME as string
+const serviceDescription = process.env.NEXT_PUBLIC_SERVICE_DESCRIPTION as string
+
+const Home: NextPage = () => {
+  return (
+    <Layout maxW="6xl">
+      <Metatag title={serviceName} description={serviceDescription} />
+      <Portfolio />
+    </Layout>
+  );
+};
+
+export default Home;
