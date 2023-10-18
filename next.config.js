7f8e5bcb177f4307ba0446181ecd1133500d2f39
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-18 11:47:02 +0900

    chore: config調整

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-const baseUrl = process.env.NEXT_PUBLIC_WALLET_BASE_URL
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: isProd ? baseUrl : undefined,
+  basePath: '',
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
