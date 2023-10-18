f46a65a63fc0debade888ea324c0e9d366ccaa21
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-10-18 16:58:34 +0900

    chore: config調整

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: '../',
+  basePath: '/e-portfolio',
+  assetPrefix: isProd ? '../' : undefined,
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
