0bfcc7f09f32afe6fffa91bda329c148f0b7eae3
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-11-13 16:21:40 +0900

    chore: basePath変更

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
   experimental: {
     outputStandalone: true,
   },
-};
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
+  async rewrites() {
+    return [
+      {
+        source: "/portfolio/api/:path*",
+        destination: "/api/:path*",
+      },
+      {
+        source: "/portfolio/images/:query*",
+        destination: '/_next/image/:query*'
+      },
+      {
+        source: "/portfolio/_next/:path*",
+        destination: "/_next/:path*",
+      },
+    ]
+  }  };
 
 module.exports = nextConfig;
