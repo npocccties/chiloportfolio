2d456d758af95b1d8a08cf2a6f477101f0bd5db3
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-11-15 13:25:44 +0900

    chore: 元に戻す

--- next.config.js
+++ next.config.js
@@ -1,62 +1,11 @@
 /** @type {import('next').NextConfig} */
-
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-const commonConfig = {
+const nextConfig = {
   reactStrictMode: true,
   basePath: "/portfolio",
   assetPrefix: "/portfolio",
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true }
-    return config
-  },
   experimental: {
     outputStandalone: true,
   },
-}
-
-const devConfig = {
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ]
-  },
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          basePath: false,
-          source: '/:path*',
-          destination: 'http://localhost:3000/:path*',
-        }
-      ]
-    };
-  },
-}
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      ...commonConfig,
-      ...devConfig,
-    };
-  }
-
-  return {
-    ...commonConfig,
-    // output: 'standalone',
-    // distDir: 'dist',
-  }
 };
+
+module.exports = nextConfig;
