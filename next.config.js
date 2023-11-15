19d29474b5c249c9a411185c5aedd39fc7566460
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-11-15 13:54:56 +0900

    chore: basePathを消してみる

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
   experimental: {
     outputStandalone: true,
   },
