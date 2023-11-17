fe9c90b43451f3779a6ce5710459cc57807ca9c6
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-11-17 13:28:26 +0900

    chore: ローカルで /portfolio で読み込めるよう調整

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = process.env.NODE_ENV == 'production'
+// const isProduction = process.env.NODE_ENV == 'production'
+// const isProduction = true
 
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
   },
-  // basePath: "/portfolio",
-  // distDir: "dist"
-  output: isProduction ? "portfolio" : "",
-  assetPrefix: isProduction ? "/portfolio" : ""
+  // basePath: isProduction ? "/portfolio" : "",
+  //assetPrefix: isProduction ? "/portfolio" : "",
+  output: "portfolio",
+  async rewrites() {
+    return [
+      {
+        source: "/portfolio",
+        destination: "/",
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
