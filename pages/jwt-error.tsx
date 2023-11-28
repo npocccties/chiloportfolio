dabdc831a14ea1812a501a4dbb732974ea32bb06
yoshinobu.eto <yoshinobu_eto@intelligent-design.co.jp>
2023-11-28 11:54:25 +0900

    chore: アラートのメッセージ変更

--- pages/jwt-error.tsx
+++ pages/jwt-error.tsx
@@ -1,6 +1,6 @@
 const JwtError = () => {
   if (typeof window !== 'undefined') {
-    window.alert('ユーザー情報の取得に失敗しました。タブを閉じます。')
+    window.alert('ユーザー情報の取得に失敗しました。タブを閉じます。\n※ 直接URL入力してe-ポートフォリオを開いた場合は手動でタブを閉じてください。')
     window.close()
   }
   return null
