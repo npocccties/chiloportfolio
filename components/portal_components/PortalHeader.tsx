1a0389cb0710a0ebc0a8562fe1c90b7e401831c7
r-matsufuji <r-matsufuji@intelligent-design.co.jp>
2025-05-20 20:49:29 +0900

    ヘッダの項目名を、.env設定可能とする対応

--- components/portal_components/PortalHeader.tsx
+++ components/portal_components/PortalHeader.tsx
@@ -13,7 +13,7 @@ import AllBadge from "public/portal/all-badge.svg";
 import Issuer from "public/portal/issuer.svg"; // SVGRで読み込み済み
 
 import Menu from "./Menu";
-import { logoLink, dashboardLink, learningLink, issuerInfos } from "./urls";
+import { logoLink, dashboardTitle, dashboardLink, learningTitle, learningLink, issuerTitle, issuerInfos } from "./urls";
 import useDialog from "./use-dialog";
 
 // import { BsWallet2 } from "react-icons/bs";
@@ -90,7 +90,7 @@ export const PortalHeader: React.FC<Props> = ({ showContents }) => {
                   href={dashboardLink}
                   className="hidden md:inline-flex jumpu-text-button text-white text-sm hover:bg-gray-700 items-center gap-2 whitespace-nowrap"
               >
-                ダッシュボード
+                {dashboardTitle}
               </Link>
             </Flex>
             <Flex gap={flexMenuItemInnerGap} alignItems={"center"} color={"basic.white"} display={{ base: "none", md: "flex" }} >
@@ -99,7 +99,7 @@ export const PortalHeader: React.FC<Props> = ({ showContents }) => {
                   href={learningLink}
                   className="hidden lg:inline-flex jumpu-text-button text-white text-sm hover:bg-gray-700 items-center gap-2 whitespace-nowrap"
                   >
-                  学びを探す
+                  {learningTitle}
               </Link>
             </Flex>
             {/*
@@ -112,7 +112,7 @@ export const PortalHeader: React.FC<Props> = ({ showContents }) => {
               title={
                 <>
                 <Issuer className="fill-white size-[1.125rem]" alt="" />
-                <span>発行元</span>
+                <span>{issuerTitle}</span>
                 </>
               }
               >
